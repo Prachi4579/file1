05f708bc525098846d85e02ecd31380ecf837809
DELL <DELL@ChikusLAPTOP>
2023-03-19 15:28:41 +0530

    typecasting2

---
+++ src/file1/hw3_2.java
@@ -0,0 +1,11 @@
+package file1;
+
+public class hw3_2 {public static void main(String[] args) {
+	//byte b1 = (byte)250;
+	//byte b1 = (byte)120;
+	//byte b1 = (byte)130;
+	byte b1 = (byte)300;
+	System.out.println(b1);
+}
+
+}
