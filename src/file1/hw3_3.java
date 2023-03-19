1dad00fa62bff34639ffa1dcb5779e19d983234c
DELL <DELL@ChikusLAPTOP>
2023-03-19 15:39:55 +0530

    octal,hexadeci to decimal

---
+++ src/file1/hw3_3.java
@@ -0,0 +1,12 @@
+package file1;
+
+public class hw3_3 {public static void main(String[] args) {
+	int i = 020;
+	int j = 20;
+	int k = 0X20;
+	System.out.println(i);
+	System.out.println(j);
+	System.out.println(k);
+}
+
+}
