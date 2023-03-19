ed526d6f97d48f20ed3b3a67a7ab637e6429ccff
DELL <DELL@ChikusLAPTOP>
2023-03-19 15:50:00 +0530

    typecast3

---
+++ src/file1/hw3_4.java
@@ -0,0 +1,16 @@
+package file1;
+
+public class hw3_4 {public static void main(String[] args) {
+	int i = 10;
+
+	int j= 7; 
+
+	//double db1 = i/j; 
+
+	//double db1 = (double)i/j; 
+
+	double db1 = i/(double)j;
+	System.out.println(db1);
+}
+
+}
