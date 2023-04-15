cba5cf40dc1208ce560f6d01f3c869a7a50128d0
DELL <DELL@ChikusLAPTOP>
2023-04-15 23:44:25 +0530

    armstrong

---
+++ src/file1/ArmStrongNumber.java
@@ -0,0 +1,30 @@
+package file1;
+
+import java.util.Scanner;
+
+
+public class ArmStrongNumber {
+	
+public static void main(String[] args) {
+Scanner scan=new Scanner(System.in);
+System.out.println("Input your number ");
+long n=scan.nextLong();
+long sum=0;
+
+long m=n;
+while(n!=0)
+{long i=n%10;    
+sum=sum+(i*i*i);
+n=n/10;
+System.out.println("Sum " + sum + " i " + i + " n " + n );
+}
+if(sum==m)
+{System.out.println("It is an Armstrong no");
+}
+else
+{System.out.println("Not an armstrong no");}
+}
+
+	
+
+}
