424a606932a7b56436d433a13854adcf94dbcc2a
DELL <DELL@ChikusLAPTOP>
2023-05-22 18:40:37 +0530

    Extend

---
+++ src/inheritance/Parent.java
@@ -0,0 +1,21 @@
+package inheritance;
+
+public class Parent {
+ void test() {
+	 
+ }
+}
+class Child extends Parent{
+	public static void main(String[] args) {
+		Child c1=new Child();
+		c1.test();
+		
+	}
+	void Food() {
+//		Parent p1=new Parent();
+//		p1.test();
+		test();
+	}
+	
+	
+}
